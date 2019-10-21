1471c2067c6baf2d47f85810b656a3cdda939e5f
Afonso Santos <55449265+afosantos1@users.noreply.github.com>
2019-10-21 19:46:47 -0400

    Update diagonal.js

--- src/diagonal.js
+++ src/diagonal.js
@@ -1,1 +1,11 @@
+const readlineSync = require("readline-sync");
 
+let width_in = Number(readlineSync.question("\nWidth: "));
+let length_in = Number(readlineSync.question("Length: "));
+
+let diagonal = Math.hypot(width_in, length_in);
+diagonal = diagonal.toFixed(2);
+diagonal = diagonal.toLocaleString();
+
+
+console.log("\nA(n) " + width_in + "-by-" + length_in + "-inch sheet of paper has diagonal of " + diagonal + " inch(es).\n" );
