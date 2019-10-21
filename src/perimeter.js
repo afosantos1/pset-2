774bbf13ec7ebfe6ce7c220ca20750a3fd495bc4
Afonso Santos <55449265+afosantos1@users.noreply.github.com>
2019-10-21 19:08:47 -0400

    Create perimeter.js

---
+++ src/perimeter.js
@@ -0,0 +1,15 @@
+  
+const readlineSync = require("readline-sync");
+
+const INCHES_TO_CM = 2.54;
+
+let width_in = Number(readlineSync.question("\nWidth: "));
+let length_in = Number(readlineSync.question("Length: "));
+
+let width_cm = width_in * INCHES_TO_CM;
+let length_cm = length_in * INCHES_TO_CM;
+let perimeter = (2 * length_cm) + (2 * width_cm);
+perimeter = perimeter.toFixed(2);
+perimeter = perimeter.toLocaleString();
+
+console.log("\nA(n) " + width_in + "-by-" + length_in + "-inch sheet of paper has perimeter of " + perimeter + " centimeter(s).\n" );
