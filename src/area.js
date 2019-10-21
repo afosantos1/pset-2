27363a7255c15dcf169f9ac8edf208ee001566e6
Afonso Santos <55449265+afosantos1@users.noreply.github.com>
2019-10-21 19:02:43 -0400

    Update area.js

--- src/area.js
+++ src/area.js
@@ -1,1 +1,14 @@
 
+const readlineSync = require("readline-sync");
+
+const INCHES_TO_MM = 25.4;
+let width_in = Number(readlineSync.question("\nWidth: "));
+let length_in = Number(readlineSync.question("Length: "));
+
+let width_mm = width_in * INCHES_TO_MM;
+let length_mm = length_in * INCHES_TO_MM;
+let area = width_mm * length_mm;
+area = area.toFixed(2);
+area = area.toLocaleString();
+
+console.log("\nA(n) " + width_in + "-by-" + length_in + "-inch sheet of paper has an area of " + area + " square millimeter(s).\n" );
