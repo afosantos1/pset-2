a946426ad109e11582482e246917aae8471e5f03
Afonso Santos <55449265+afosantos1@users.noreply.github.com>
2019-10-25 09:38:50 -0400

    Update salary.js

--- src/salary.js
+++ src/salary.js
@@ -1,11 +1,11 @@
 const readlineSync = require("readline-sync");
 
 const PRETAX = 0.07;
-const FEDERAL_INCOME_TAX = 0.157;
-const STATE_INCOME_TAX = 0.0447;
-const SOCIAL_SECURITY_TAX = 0.062;
-const MEDICARE_TAX = 0.0145;
-const PAY_PERIODS_PER_YEAR = 24;
+const FEDERAL_income_tax = 0.157;
+const STATE_income_tax = 0.0447;
+const SOCIAL_security_tax = 0.062;
+const MEDICARE_tax = 0.0145;
+const PAY_periods_per_year = 24;
 
 
 let userAnnualSalary = Number(readlineSync.question("\nAnnual Salary: "));
@@ -13,9 +13,9 @@ let userAnnualSalary = Number(readlineSync.question("\nAnnual Salary: "));
 
 let preTaxSalary = userAnnualSalary - (userAnnualSalary * PRETAX);
 console.log(preTaxSalary);
-let totalDeductionPercentage = FEDERAL_INCOME_TAX + STATE_INCOME_TAX + SOCIAL_SECURITY_TAX + MEDICARE_TAX;
+let totalDeductionPercentage = FEDERAL_income_tax + STATE_income_tax + SOCIAL_security_tax + MEDICARE_tax;
 console.log(totalDeductionPercentage);
-takeHomePay = (preTaxSalary - (preTaxSalary * totalDeductionPercentage)) / PAY_PERIODS_PER_YEAR;
+takeHomePay = (preTaxSalary - (preTaxSalary * totalDeductionPercentage)) / PAY_periods_per_year;
 console.log(takeHomePay);
 takeHomePay = takeHomePay.toLocaleString("en", { style: "currency", currency: "USD" });
 
