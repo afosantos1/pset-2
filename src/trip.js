3263c0c0f2a2de3c4a03f0044a022df890ce1a53
Afonso Santos <55449265+afosantos1@users.noreply.github.com>
2019-10-24 20:42:00 -0400

    Update trip.js

--- src/trip.js
+++ src/trip.js
@@ -1,1 +1,13 @@
+const readlineSync = require("readline-sync");
 
+const STUDENTS = Number(readlineSync.question("\nStudents: "));
+const TEACHERS = Number(readlineSync.question("Teachers: "));
+const CAPACITY = Number(readlineSync.question("Bus capacity: "));
+
+let totalPeople = TEACHERS + STUDENTS;
+let totalBuses = Math.ceil(totalPeople / CAPACITY);
+let peopleOnLastBus = totalPeople % CAPACITY;
+
+console.log("\n" + totalBuses + " bus(es) is (are) needed, with " + peopleOnLastBus + " passenger(s) on the last bus.\n");
+
+//what if even number of people fit in the bus
