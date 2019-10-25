c783d172c5982af8c5207b33a4549a0d56313076
Afonso Santos <55449265+afosantos1@users.noreply.github.com>
2019-10-24 20:45:56 -0400

    Update cornhole.js

--- src/cornhole.js
+++ src/cornhole.js
@@ -8,6 +8,8 @@ let Areaofboard = LENGTH * WIDTH;
 let radius = DIAMETER / 2;
 let Areaofcircle = Math.PI * (radius ** 2);
 
+
+
 let cornholeSurfaceArea = Areaofboard - Areaofcircle;
 cornholeSurfaceArea = cornholeSurfaceArea.toLocaleString("en", {maximumFractionDigits: 2, minimumFractionDigits: 2});
 console.log("The surface area of a standard Cornhole board is " + cornholeSurfaceArea + " square inch(es).");
