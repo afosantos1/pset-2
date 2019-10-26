968609a05ffa50af470b56694a0a8588d074386e
Afonso Santos <55449265+afosantos1@users.noreply.github.com>
2019-10-26 16:02:47 -0400

    Update hourly.js

--- src/hourly.js
+++ src/hourly.js
@@ -2,7 +2,7 @@ const readlineSync = require("readline-sync");
 
 const HOURLY_WAGE = Number(readlineSync.question("\nHourly wage: "));
 
-//user's work hours for each day of the week
+
 let Hoursmonday = Number(readlineSync.question("\nMonday: "));
 let Hourstuesday = Number(readlineSync.question("Tuesday: "));
 let Hourswednesday = Number(readlineSync.question("Wednesday: "));
@@ -11,7 +11,7 @@ let Hoursfriday = Number(readlineSync.question("Friday: "));
 let Hourssaturday = Number(readlineSync.question("Saturday: "));
 let Hourssunday = Number(readlineSync.question("Sunday: "));
 
-//the money the user makes each day based on their hours and hourly wage
+
 let mondayEarnings = Hoursmonday * HOURLY_WAGE;
 let tuesdayEarnings = Hourstuesday * HOURLY_WAGE;
 let wednesdayEarnings = Hourswednesday * HOURLY_WAGE;
@@ -20,7 +20,7 @@ let fridayEarnings = Hoursfriday * HOURLY_WAGE;
 let saturdayEarnings = Hourssaturday * HOURLY_WAGE;
 let sundayEarnings = Hourssunday * HOURLY_WAGE;
 
-//the total money the user earns, which is the sum of their daily earnings
+
 let totalWeekSalary = mondayEarnings + tuesdayEarnings + wednesdayEarnings + thursdayEarnings + fridayEarnings + saturdayEarnings + sundayEarnings;
 totalWeekSalary = totalWeekSalary.toLocaleString("en", { style: "currency", currency: "USD" });
 
